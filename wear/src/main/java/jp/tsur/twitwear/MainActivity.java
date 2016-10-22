962ea7e80ab7e69207f9cdc9068d1f70db709cdd
teshi04 <teshi04@gmail.com>
2016-10-23 04:19:49 +0900

    Add navigation drawer

--- wear/src/main/java/jp/tsur/twitwear/MainActivity.java
+++ wear/src/main/java/jp/tsur/twitwear/MainActivity.java
@@ -1,10 +1,13 @@
 package jp.tsur.twitwear;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.drawer.WearableNavigationDrawer;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 
 import jp.tsur.twitwear.databinding.ActivityMainBinding;
@@ -25,6 +28,10 @@ public class MainActivity extends WearableActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.navigationDrawer.setAdapter(new NavigationAdapter());
+        binding.drawerLayout.peekDrawer(Gravity.TOP);
+
         binding.list.setLayoutManager(new LinearLayoutManager(this));
         binding.list.hasFixedSize();
         binding.list.addItemDecoration(new DividerDecoration(this));
@@ -62,5 +69,40 @@ public class MainActivity extends WearableActivity {
                         binding.list.setAdapter(adapter);
                     }
                 });
+    }
+
+    private class NavigationAdapter extends WearableNavigationDrawer.WearableNavigationDrawerAdapter {
+
+        @Override
+        public String getItemText(int position) {
+            switch (position) {
+                case 0:
+                    return "Home";
+                case 1:
+                    return "Notification";
+            }
+            return null;
+        }
+
+        @Override
+        public Drawable getItemDrawable(int position) {
+            switch (position) {
+                case 0:
+                    return getDrawable(R.drawable.home_vector);
+                case 1:
+                    return getDrawable(R.drawable.notification_vector);
+            }
+            return null;
+        }
+
+        @Override
+        public void onItemSelected(int position) {
+
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 }
